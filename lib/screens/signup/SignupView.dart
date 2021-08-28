ccd72c7cb06ac9fcb3077723b2aa8e20346e8feb
Eliseu Wesley <32180529+ZeusWesley@users.noreply.github.com>
2021-08-28 02:21:46 -0300

    signup view features

--- lib/screens/signup/SignupView.dart
+++ lib/screens/signup/SignupView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pico/components/AppButton/AppButton.dart';
+import 'package:pico/components/AppField/AppField.dart';
 import 'package:pico/components/MainLayout/MainLayout.dart';
 import 'package:pico/util/colors.dart';
 
@@ -11,6 +12,22 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupViewState extends State<SignupView> {
+  bool acceptedTerms = false;
+
+  Widget label(String label) {
+    return Padding(
+      padding: EdgeInsets.only(top: 16.0, bottom: 8.0),
+      child: Text(
+        label,
+        style: TextStyle(
+          color: kLabelColor,
+          fontWeight: FontWeight.w700,
+          fontSize: 14.0,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MainLayout(
@@ -29,34 +46,61 @@ class _SignupViewState extends State<SignupView> {
             ),
             SizedBox(width: 20),
             AppButton(label: "Entrar"),
-            SizedBox(width: 20)
+            SizedBox(width: 20),
           ],
         )
       ],
       child: Container(
         padding: EdgeInsets.all(24.0),
-        child: Flex(
-          direction: Axis.vertical,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Crie sua conta.",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 24.0,
+        child: SingleChildScrollView(
+          child: Flex(
+            direction: Axis.vertical,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Crie sua conta.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24.0,
+                ),
               ),
-            ),
-            AppButton(label: "Criar sua conta com Google"),
-            AppButton(label: "Criar sua conta com Facebook"),
-            Text(
-              "Ou crie sua conta usando um email.",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 16.0,
+              SizedBox(height: 20.0),
+              AppButton(label: "Criar sua conta com Google"),
+              SizedBox(height: 16.0),
+              AppButton(label: "Criar sua conta com Facebook"),
+              SizedBox(height: 20.0),
+              Text(
+                "Ou crie sua conta usando um email.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.0,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 4.0),
+              label("Nome de usuário:"),
+              AppField(hint: "Insira o nome que deseja utilizar"),
+              label("Seu email:"),
+              AppField(hint: "Insira seu email"),
+              label("Sua senha:"),
+              AppField(hint: "Insira a senha que deseja criar"),
+              Row(
+                children: [
+                  Checkbox(
+                    value: acceptedTerms,
+                    onChanged: (val) {
+                      setState(() {
+                        acceptedTerms = val;
+                      });
+                    },
+                  ),
+                  Text(
+                      "Estou ciente dos Termos de Uso e\nPolíticas de privacidade do Onde é o pico")
+                ],
+              ),
+              AppButton(label: "Continuar")
+            ],
+          ),
         ),
       ),
     );
--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF0055FF);
 const kTextColor = Color(0xFF000000);
-const kBackgroundColor = Color(0xFFE5E5E5);
+const kLabelColor = Color(0xFF6C757D);
+const kBackgroundColor = Color(0xFFFAFAFA);
