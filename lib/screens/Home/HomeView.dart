d5817d7d3186a11062a6aae62042b95e6a9a6811
Eliseu Wesley <32180529+ZeusWesley@users.noreply.github.com>
2021-08-28 01:27:10 -0300

    renaming home view

---
+++ lib/screens/Home/HomeView.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:pico/components/SearchBar/SearchBar.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pico/util/mapStyle.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView({Key key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  GoogleMapController mapController;
+  // final _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  // static final CameraPosition _kLake = CameraPosition(
+  //     bearing: 192.8334901395799,
+  //     target: LatLng(37.43296265331129, -122.08832357078792),
+  //     tilt: 59.440717697143555,
+  //     zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF343332),
+      body: Container(
+        margin: EdgeInsets.only(top: 30.0),
+        child: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+                mapController.setMapStyle(MapStyle.main);
+              },
+            ),
+            SearchBar(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Color(0xFF8BBEFF),
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.explore_outlined),
+            label: 'HomeView',
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.flag_outlined),
+            label: 'Desafios',
+          ),
+        ],
+      ),
+    );
+  }
+}
