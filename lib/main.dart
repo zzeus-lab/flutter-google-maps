090ab06a88796cbc90c43b0f2b63d71a09beaaa4
Eliseu Wesley <32180529+ZeusWesley@users.noreply.github.com>
2021-08-28 01:27:48 -0300

    draft to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pico/screens/Home/home.dart';
-import 'package:pico/util/themeColors.dart';
+import 'package:pico/screens/home/HomeView.dart';
+import 'package:pico/screens/signup/SignupView.dart';
+import 'package:pico/util/colors.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle(
-          statusBarColor:
-              Colors.black, //set as per your  status bar color
+          statusBarColor: Colors.black, //set as per your  status bar color
           systemNavigationBarColor:
               Colors.black, //set as per your navigation bar color
           statusBarIconBrightness:
@@ -22,12 +22,20 @@ class MyApp extends StatelessWidget {
           systemNavigationBarIconBrightness: Brightness.dark,
         ),
         child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Pico',
-          theme: ThemeData(
-            primarySwatch: ThemeColors.primary,
-          ),
-          home: Home(),
-        ));
+            debugShowCheckedModeBanner: false,
+            title: 'Pico',
+            // initialRoute: "/signup",
+            // routes: {
+            //   '/': (context) => HomeView(),
+            //   '/signup': (context) => SignupView(),
+            // },
+            theme: ThemeData(
+              scaffoldBackgroundColor: kBackgroundColor,
+              primaryColor: kPrimaryColor,
+              textTheme:
+                  Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: SignupView()));
   }
 }
