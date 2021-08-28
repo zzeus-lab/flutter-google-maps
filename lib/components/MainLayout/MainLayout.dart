ac2051c2119a944e288dc691c03996e7b35c52ce
Eliseu Wesley <32180529+ZeusWesley@users.noreply.github.com>
2021-08-28 01:29:22 -0300

    main layout with app bar

--- lib/components/MainLayout/MainLayout.dart
+++ lib/components/MainLayout/MainLayout.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pico/components/AppButton/AppButton.dart';
+import 'package:pico/util/colors.dart';
 
 class MainLayout extends StatefulWidget {
-  MainLayout({Key key}) : super(key: key);
+  final Widget child;
+  final List<Widget> actions;
+  MainLayout({this.child, this.actions});
 
   @override
   _MainLayoutState createState() => _MainLayoutState();
@@ -10,8 +15,18 @@ class MainLayout extends StatefulWidget {
 class _MainLayoutState extends State<MainLayout> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/logo/pico.svg"),
+          onPressed: () {},
+        ),
+        leadingWidth: 80,
+        actions: widget.actions,
+      ),
+      body: widget.child,
     );
   }
 }
